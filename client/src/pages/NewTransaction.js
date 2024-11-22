c91d49258b1fbd7e7eb111af8ebf73d94f9b2676
Taanishq <taanishqsethi28@gmail.com>
2024-11-22 15:15:04 -0800

    Checking github

--- client/src/pages/NewTransaction.js
+++ client/src/pages/NewTransaction.js
@@ -1,3 +1,10 @@
 //Menu to log new transaction
 //Transaction Type to be sepcified in parameters on landing page (Spending, Income, Transfer, Pay Credit)
 // If spending, amount inputted is subtracted from specified wallet balance or added to credit balance
+
+import { React, useEffect, useState } from 'react';
+import axios from "axios";
+
+const NewTransaction = () => {
+    
+}
