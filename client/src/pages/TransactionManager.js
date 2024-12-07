2b30aa5e499cd5b1a0e4795663ed46d5fdcd0679
Taanishq <taanishqsethi28@gmail.com>
2024-12-06 21:04:55 -0800

    Put the correct path for Transaction manager page for return button

--- client/src/pages/TransactionManager.js
+++ client/src/pages/TransactionManager.js
@@ -149,7 +149,7 @@ const TransactionManager = () => {
             </Link>
 
             <button 
-                    onClick={() => navigate(-1)} 
+                    onClick={() => navigate("/")} 
                     className={styles.returnButton}
                 >
                     Return
